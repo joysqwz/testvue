e6e5e91c7aff4638daf97ba8bb35af2f80cafa62
joysqwz <joysqwzweb@gmail.com>
2024-12-12 20:44:25 +0300

    ''

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+       class="header__logo-image"
+       src="/images/logo.svg"
+       alt="логотип"
+       width="46"
+       height="46" />
+</template>
+
+<script></script>
+
+<style></style>
